62028d10a99bb4a7165364408fc054a3e4973319
Adesh856 <adeshtayde6@gmail.com>
2023-07-13 14:09:13 +0530

    first

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 module.exports= async(req,res,next)=>{
-     let token = req.headers?.authorization.split(" ")[1]
+     let token = req.headers?.authorization?.split(" ")[1]
      if(token){
        const decoded = jwt.verify(token,"MangalYaan")
        if(decoded){
